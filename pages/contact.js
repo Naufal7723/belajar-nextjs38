5cb4c3c5c02a1d675abd491b586c83131752721f
belajarnextjs1 <naufalff128@gmail.com>
2023-11-07 09:42:26 +0700

    comit

---
+++ pages/contact.js
@@ -0,0 +1,12 @@
+import Navbar from "./Components/Navbar"
+
+function contact() {
+  return (
+    <>
+    <Navbar />
+    <img src="/next.svg" alt="Logo Next JS" width={100} height={100}></img>
+    <h1>ini halaman contact</h1>
+    </>
+  );
+}
+export default contact;
